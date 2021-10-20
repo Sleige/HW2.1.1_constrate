095934db41d2178e746eb46f9e1d8c5e6c376d7b
Vsevolod Kondratev <vsevolod.kondratev@gmail.com>
2021-10-20 22:34:17 +0700

    first commit

---
+++ HW2.1.1_constrate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1.1_constrate
+//
+//  Created by Всеволод Кондратьев on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
